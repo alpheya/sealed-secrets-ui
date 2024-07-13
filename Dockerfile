7b41afd8fb55dd3705aec5145dc3b2059cd18912
Bobby Donchev <contact@donchev.is>
2024-07-13 21:01:37 +0400

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM golang:1.22.4-bookworm as build
+WORKDIR /go/src/app
+COPY . ./
+
+ARG VERSION
+
+ENV CGO_ENABLED=0
+ENV GOOS=linux
+ENV GOARCH=amd64
+
+RUN go mod tidy \
+  && go build -o /go/bin/app -ldflags="-s -w"
+
+FROM gcr.io/distroless/static-debian11
+
+COPY --from=build /go/bin/app /
+
+CMD ["/app"]
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"time"
 
-	"github.com/quantum-wealth/sealed-secrets-ui/k8s"
-	sealedsecret "github.com/quantum-wealth/sealed-secrets-ui/sealed-secret"
 	"github.com/quantum-wealth/sealed-secrets-ui/web"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -27,26 +24,6 @@ func setupLogging() {
 
 func main() {
 	setupLogging()
-	pubKey, err := k8s.GetPublicKey()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	params := sealedsecret.EncryptValuesParams{
-		PubKey:    pubKey,
-		Namespace: "default",
-		Scope:     "",
-		Name:      "my-sealed-secret",
-		Values:    map[string]string{"password": "supersecret"},
-	}
-
-	yamlManifest, err := sealedsecret.GetSealedSecret(params)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Println(yamlManifest)
-
 	handler := web.NewHandler()
 
 	web.Start("8080", handler)
